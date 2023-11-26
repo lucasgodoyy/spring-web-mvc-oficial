e808b03f4a3ebe9cfd092a907f1a22c3e786a071
lucasgodoyy <ehlucasgodoy10@gmail.com>
2023-11-26 00:41:04 -0300

    Criando entidade Pessoa, controller e service

---
+++ src/main/java/br/com/lucasdeveloper/springboot/repository/PessoaRepository.java
@@ -0,0 +1,16 @@
+package br.com.lucasdeveloper.springboot.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.lucasdeveloper.springboot.model.Pessoa;
+
+
+@Repository
+@Transactional
+public interface PessoaRepository extends CrudRepository<Pessoa, Long>{
+
+	
+	
+}
