c67544c932e44d3c1786d28b9a38f519bc3c1268
lucasgodoyy <ehlucasgodoy10@gmail.com>
2023-11-27 19:40:08 -0300

    Role

--- src/main/java/br/com/lucasdeveloper/springboot/security/WebConfigSecurity.java
+++ src/main/java/br/com/lucasdeveloper/springboot/security/WebConfigSecurity.java
@@ -1,5 +1,6 @@
 package br.com.lucasdeveloper.springboot.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -15,12 +17,16 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+	
 	@Override // Configura as solicitações de acesso por Http
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf()
 		.disable() // Desativa as configurações padrão de memória
 		.authorizeRequests() // Permite restringir acesso
 		.antMatchers(HttpMethod.GET, "/").permitAll() // Qualquer usuário acessa a página inicial
+		.antMatchers(HttpMethod.GET, "/cadastroPessoa").hasAnyRole("GERENTE")
 		.anyRequest().authenticated()
 		.and().formLogin().permitAll() // permite qualquer usuário
 		.and().logout() // Mapeia URL de Logout e invalida usuário autenticado
@@ -31,17 +37,18 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 	@Override // Cria autenticação do usuário com banco de dados ou em memória
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
+		
+		/*	auth.userDetailsService(implementacaoUserDetailsService);*/
+		
+		
+		
 		// AUTENTICAÇÃO VIA BANCO DE DADOS
 		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
 		.withUser("lucas")
 		.password("123")
 		.roles("ADMIN");
 		
-		// AUTENTICAÇÃO EM MEMÓRIA
-		/*auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-		.withUser("tadeu")
-		.password("$2a$10$Q/AyB6h3cIT2QVaTvejBruAV7xoQLFdi7tRm2aAA0URqDxVJj6Lvm") // 123
-		.roles("ADMIN");*/
+		
 	}
 	
 	@Override // Ignora URL's específicas
